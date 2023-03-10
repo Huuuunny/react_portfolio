d7c67a8fe18817dfb4ca6b124c1e8082f4ecfe8d
huuuunny <doucet.yohann@gmail.com>
2023-03-10 13:46:13 +0100

    edit footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,8 +14,8 @@ const Footer = () => {
       <a href="#" className='footer_logo'>Yohann Doucet</a>
       <ul className='permalinks'>
         <li><a href="#">Accueil</a></li>
-        <li><a href="#about">A propos</a></li>
-        <li><a href="#experience">Experience</a></li>
+        <li><a href="#about">Mon parcours</a></li>
+        <li><a href="#experience">Mes compétences</a></li>
         {/* <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li> */}
