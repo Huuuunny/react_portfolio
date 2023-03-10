6d176af0a5790f2d585d2c74e15b130e87b45311
huuuunny <doucet.yohann@gmail.com>
2023-03-10 13:43:03 +0100

    traduction footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,8 +13,8 @@ const Footer = () => {
     <footer>
       <a href="#" className='footer_logo'>Yohann Doucet</a>
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#about">A propos</a></li>
         <li><a href="#experience">Experience</a></li>
         {/* <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
